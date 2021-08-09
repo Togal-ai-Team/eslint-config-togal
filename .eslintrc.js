73d91bbd97e2a4d92dd8925e3e24889e67feb035
Marat A. Denenberg <github@revnode.com>
2021-08-09 15:23:41 -0400

    Indent inside of a switch, default is 0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "indent": ["error", 2],
+    "indent": ["error", 2, {"SwitchCase": 1}],
     "linebreak-style": [
       "error",
       "unix"
